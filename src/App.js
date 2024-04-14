ca399cebdce337e33fc597cb2fc738e411d98d06
ashish <1508ramashish@gmail.com>
2024-04-14 10:54:17 +0530

    tellintime

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState ,useEffect} from "react";
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    // Update the current time every second
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Clean up the interval to avoid memory leaks
+    return () => clearInterval(interval);
+  }, []);
+ 
+  const handleclick=()=>{
+    console.log("onclick is call ")
+    if ('speechSynthesis' in window) {
+      var date =new Date();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+    // Create a new SpeechSynthesisUtterance object with the text to be spoken
+    
+    var timeformate= "hello vardaan abi time hua hai " + hours +"baj k " + minutes+ "minutes ";
+    console.log(date);
+    var utterance = new SpeechSynthesisUtterance(timeformate);
+    utterance.rate = 0.7;
+    utterance.lang = 'hi-IN';
+    // Use the speechSynthesis object to speak the utterance
+    window.speechSynthesis.speak(utterance);
+} else {
+    console.log('Speech synthesis not supported');
+}
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 style={{fontSize: "56px", textAlign: "center", color:'orange'}}>Current Time</h1>
+      <div style={{ fontSize: "64px", textAlign: "center" ,color:'blue' }}>
+        {currentTime.toLocaleTimeString()}
+        <div>
+          <br></br>
+          <button onClick={handleclick} style={{width:'2cm', cursor:'pointer',height:'1.5cm',fontSize:'15px' ,border:'solid green 6px', borderRadius:'10px' , backgroundColor:'yellow' ,color:'blue'}} > SPEAK</button> 
+        </div>
+      </div>
     </div>
-  );
+ 
+  )
 }
 
 export default App;
