c53c727cc4598d1fd3025a96e6117ee31d4f7df7
ashish <1508ramashish@gmail.com>
2024-04-14 17:48:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState ,useEffect} from "react";
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date());
   const [btnclr,setbtnclr]=useState('gray')
+  const [start,setstart]=useState(true);
 
   useEffect(() => {
     // Update the current time every second
@@ -12,7 +13,34 @@ function App() {
     // Clean up the interval to avoid memory leaks
     return () => clearInterval(interval);
   }, []);
- 
+ const automate=(par)=>{
+  if(par===true){
+    setstart(false);
+    console.log(
+      "automate function run"
+        )
+    
+      if ('speechSynthesis' in window) {
+        var date =new Date();
+        var hours = date.getHours();
+        if(hours>12){
+          hours=hours-12;
+        }
+        const minutes = date.getMinutes();
+      var timeformate= "hello vardaan abi time hua hai " + hours +"baj k " + minutes+ "minute";
+     
+      var utterance = new SpeechSynthesisUtterance(timeformate);
+      utterance.rate = 0.8;
+      utterance.lang = 'hi-IN';
+      window.speechSynthesis.speak(utterance);
+    } else {
+      console.log('Speech synthesis not supported');
+    }
+   
+    
+   }
+  }
+  
   const handleclick=()=>{
     if(btnclr==='gray')
    {
@@ -23,7 +51,10 @@ function App() {
     console.log("onclick is call ")
     if ('speechSynthesis' in window) {
       var date =new Date();
-      const hours = date.getHours();
+      var hours = date.getHours();
+      if(hours>12){
+        hours=hours-12;
+      }
       const minutes = date.getMinutes();
     // Create a new SpeechSynthesisUtterance object with the text to be spoken
     
@@ -42,9 +73,11 @@ function App() {
     <div className="container">
       <h1 style={{fontSize: "56px", textAlign: "center", color:'orange'}}>Current Time</h1>
       <div style={{ fontSize: "64px", textAlign: "center" ,color:'blue' }}>
+      {automate(start)}
         {currentTime.toLocaleTimeString()}
         <div>
           <br></br>
+          
           <button onClick={handleclick} style={{width:'2cm', cursor:'pointer',height:'1.5cm',fontSize:'15px' ,border:'solid green 6px', borderRadius:'10px' , backgroundColor:btnclr ,color:'blue'}} > SPEAK</button> 
         </div>
       </div>
